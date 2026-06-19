276b2f0b51f7fa3fa3a1f6218c0dafe642640c60
Leon W. <orlando.game@web.de>
2026-06-19 18:20:55 +0200

    added: chickens, clouds and first background img to the project

---
+++ models/cloud.class.js
@@ -0,0 +1,14 @@
+class Cloud extends MovableObject {
+    y = 20;
+    height = 250;
+    width = 500;
+
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+
+
+        this.x = 220 + Math.random() * 500;
+        
+    }
+}
