92f6482bc525141708f4f16ca604848b0d993e3d
Leon W. <orlando.game@web.de>
2026-06-19 18:55:39 +0200

    refactored: updated dimensions and constructor parameters for BackgroundObject, Character, and Chicken classes; adjusted y position in MovableObject; added multiple background layers in World class

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,10 @@
-class Character extends MovableObject{
+class Character extends MovableObject {
+    x = 120;
+    y = 180;
+    height = 250;
+    width = 130;
 
-
-    constructor(){
+    constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
 
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,10 +1,12 @@
 class Chicken extends MovableObject {
+    y = 350 + Math.random() * 35;
+    height = 70;
+    width = 75;
 
-
-    constructor(){
+    constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-    
-    
+
+
         this.x = 220 + Math.random() * 500;
     }
 
