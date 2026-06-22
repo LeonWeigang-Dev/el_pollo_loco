169f46ae835f7fef69ed8382485f2bfcd4cdcfbc
Leon W. <orlando.game@web.de>
2026-06-22 17:08:59 +0200

    added: animations for the clouds, chicken and my Character

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -8,7 +8,15 @@ class Cloud extends MovableObject {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
 
-        this.x = 220 + Math.random() * 500;
-        
+        this.x = Math.random() * 500;
+        this.animate();
+
     }
+
+
+    animate() {
+        this.moveLeft();
+
+    }
+
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,17 +4,36 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.15;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png, 'img/image2.png,...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path
+            this.imageCache[path] = img;
+        })
+
+    }
+
     moveRight() {
         console.log('Moving Right')
     }
 
-        moveLeft() {
-        
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+
+        }, 1000 / 60)
     }
 }
