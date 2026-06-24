0de14e7acee76e29e3c7dbadc611e50fdd2845b9
Leon W. <orlando.game@web.de>
2026-06-24 17:30:04 +0200

    refactored: integrated keyboard controls into Character and World classes; added multiple background layers

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     y = 180;
     height = 250;
     width = 130;
+    speed = 0.15;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -11,6 +12,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
+    world;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -21,11 +23,29 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 0, Rest 0
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }, 150);
+
+            setInterval(() => {
+                if (this.world.keyboard.RIGHT) {
+                    this.x += this.speed;
+                    this.otherDirection = false;
+                }
+
+                if (this.world.keyboard.LEFT) {
+                    this.x -= this.speed;
+                    this.otherDirection = true;
+                }
+                this.world.camera_x = -this.x;
+            }, 1000 / 60);
+
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                //walk animation
+                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => 0, Rest 0
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 50);
     }
 
     jump() {
