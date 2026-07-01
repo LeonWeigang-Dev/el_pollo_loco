7944276ff51ce148e6b53974faab3d740f9566f5
Leon W. <orlando.game@web.de>
2026-07-01 18:44:18 +0200

    refactored: updated canvas element class and added responsive styles for better layout

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -5,7 +5,7 @@ class Endboss extends MovableObject {
     y = -75;
 
 
-    IMAGES_WALKING = [
+    IMAGES_ALERT = [
         'img/4_enemie_boss_chicken/2_alert/G5.png',
         'img/4_enemie_boss_chicken/2_alert/G6.png',
         'img/4_enemie_boss_chicken/2_alert/G7.png',
@@ -16,16 +16,39 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/2_alert/G12.png'
     ];
 
+    IMAGES_SPAWNING = [
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png'
+    ];
+
+    hadFirstContact = false;
+
     constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
-        this.loadImages(this.IMAGES_WALKING);
+        super().loadImage(this.IMAGES_SPAWNING[0]);
+        this.loadImages(this.IMAGES_SPAWNING);
+        this.loadImages(this.IMAGES_ALERT);
         this.x = 2350;
         this.animate();
     }
 
     animate() {
+
+        let i = 0;
         setInterval(() => {
-                this.playAnimation(this.IMAGES_WALKING);
+            if (i < 10) {
+                this.playAnimation(this.IMAGES_SPAWNING);
+            } else {
+                this.playAnimation(this.IMAGES_ALERT);
+            }
+
+            i++;
+
+            if (world.character.x > 2000 && !this.hadFirstContact) {
+                i = 0;
+                this.hadFirstContact = true;
+            }
         }, 200);
     }
 
